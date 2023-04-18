783390f2e548f7b2370c12b5bc36d6871c7deb21
Ron <ron.ebken@gmail.com>
2023-04-18 10:21:48 +0200

    gpt timer app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div class="container">
+      <h1>Timer App</h1>
+      <h2>{{ time / 100 | number: '1.2-2' }}</h2>
+      <button (click)="startTimer()" [disabled]="timerOn">Start</button>
+      <button (click)="stopTimer()" [disabled]="!timerOn">Stop</button>
+      <button (click)="resetTimer()">Reset</button>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'timer-app';
+export class AppComponent implements OnInit {
+  time: number = 0;
+  timerOn: boolean = false;
+  interval: any;
+
+  ngOnInit() {}
+
+  startTimer() {
+    this.timerOn = true;
+    this.interval = setInterval(() => {
+      this.time += 10;
+    }, 10);
+  }
+
+  stopTimer() {
+    this.timerOn = false;
+    clearInterval(this.interval);
+  }
+
+  resetTimer() {
+    this.time = 0;
+    clearInterval(this.interval);
+    this.timerOn = false;
+  }
 }
